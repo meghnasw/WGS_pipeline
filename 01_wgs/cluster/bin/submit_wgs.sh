315a5458606872b35d2f647ad6374b48f423ea9c
meghnasw <meghnasw.96@gmail.com>
2026-02-27 21:48:40 +0100

    Update submit_wgs.sh

--- 01_wgs/cluster/bin/submit_wgs.sh
+++ 01_wgs/cluster/bin/submit_wgs.sh
@@ -57,4 +57,4 @@ sbatch \
   --cpus-per-task="$CPUS" \
   --mem="$MEM" \
   --export=ALL,LINEAGE="$BUSCO_LINEAGE",BUSCO_DOWNLOADS="$BUSCO_DOWNLOADS" \
-  01_wgs/cluster/slurm/run_wgs.slurm
+  cluster/slurm/run_wgs.slurm
